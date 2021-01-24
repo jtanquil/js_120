6a867fb36415cdeb9686da37eafacdbb195fa036
jtanquil <jose.tanquilut@gmail.com>
2021-01-24 04:58:32 -0800

    Worked on RPS Extras

--- lesson_1/oo_rps.js
+++ lesson_1/oo_rps.js
@@ -1,26 +1,92 @@
+/* eslint-disable max-statements */
+/* eslint-disable max-lines-per-function */
 const readline = require('readline-sync');
 
 function createPlayer() {
   return {
     move: null,
+    moveHistory: [],
+    score: 0,
+
+    getMove () {
+      return this.move;
+    },
+
+    getMoveHistory () {
+      return this.moveHistory;
+    },
+
+    updateMoveHistory (result) {
+      this.moveHistory.push([this.getMove(), result]);
+    },
+
+    getScore () {
+      return this.score;
+    },
+
+    incrementScore () {
+      this.score += 1;
+    },
+
+    resetScore () {
+      this.score = 0;
+    },
   };
 }
 
-function createComputer() {
+function createComputer(winningMoves) {
   let playerObject = createPlayer();
+  let initialWeights = {};
+
+  for (let key in winningMoves) {
+    initialWeights[key] = 1;
+  }
 
   let computerObject = {
+    weights: initialWeights,
+
     choose () {
-      const choices = ['rock', 'paper', 'scissors'];
+      const choices = [];
+
+      for (let key in this.weights) {
+        for (let length = 0; length < this.weights[key]; length += 1) {
+          choices.push(key);
+        }
+      }
+
       let randomIndex = Math.floor(Math.random() * choices.length);
       this.move =  choices[randomIndex];
+    },
+
+    getPoints () {
+      return this.getMoveHistory().reduce((weights, round) => {
+        if (!weights.hasOwnProperty(round[0])) {
+          weights[round[0]] = 0;
+        }
+
+        if (round[1] === "W") {
+          weights[round[0]] += 1;
+        } else if (round[1] === "L") {
+          weights[round[0]] -= 1;
+        }
+
+        return weights;
+      }, {});
+    },
+
+    updateWeights () {
+      let newPoints = this.getPoints();
+
+      for (let key in this.weights) {
+        this.weights[key] = Math.max(newPoints[key], 1) || this.weights[key];
+      }
     },
   };
 
   return Object.assign(playerObject, computerObject);
 }
 
-function createHuman() {
+function createHuman(winningMoves) {
   let playerObject = createPlayer();
 
   let humanObject = {
@@ -28,10 +94,10 @@ function createHuman() {
       let choice;
 
       while (true) {
-        console.log("Choose a move ('rock', 'paper' or 'scissors'):");
+        console.log(`Choose a move (${Object.keys(winningMoves).join(", ")}):`);
         choice = readline.question();
 
-        if (['rock', 'paper', 'scissors'].includes(choice)) break;
+        if (Object.keys(winningMoves).includes(choice)) break;
 
         console.log('Sorry, invalid choice.');
       }
@@ -44,58 +110,162 @@ function createHuman() {
 }
 
 // engine object that orchestrates the objects and implements program flow
-const RPSGame = {
-  human: createHuman(),
-  computer: createComputer(),
+function createRPSGame(winningMoves) {
+  return {
+    winningMoves,
+    human: createHuman(winningMoves),
+    computer: createComputer(winningMoves),
 
-  displayWelcomeMessage() {
-    console.log('Welcome to Rock, Paper, Scissors!');
-  },
+    displayWelcomeMessage() {
+      console.log('Welcome to Rock, Paper, Scissors!');
+      console.log('Each round is worth one point. First player to 5 points wins!');
+    },
 
-  displayGoodbyeMessage() {
-    console.log(`Thanks for playing Rock, Paper, Scissors!`);
-  },
+    displayGoodbyeMessage() {
+      console.log(`Thanks for playing Rock, Paper, Scissors!`);
+    },
 
-  displayWinner() {
-    let humanMove = this.human.move;
-    let computerMove = this.computer.move;
+    displayRoundWinner(roundWinner) {
+      let humanMove = this.human.getMove();
+      let computerMove = this.computer.getMove();
 
-    console.log(`You chose: ${humanMove}`);
-    console.log(`The computer chose: ${computerMove}`);
+      console.log(`You chose: ${humanMove}`);
+      console.log(`The computer chose: ${computerMove}`);
 
-    if ((humanMove === 'rock' && computerMove === 'scissors') ||
-      (humanMove === 'scissors' && computerMove === 'paper') ||
-      (humanMove === 'paper' && computerMove === 'rock')) {
-      console.log('You win!');
-    } else if ((humanMove === 'rock' && computerMove === 'paper') ||
-      (humanMove === 'paper' && computerMove === 'scissors') ||
-      (humanMove === 'scissors' && computerMove === 'rock')) {
-      console.log('Computer wins!');
-    } else {
-      console.log("It's a tie");
-    }
-  },
+      if (roundWinner === 'human') {
+        console.log("You won!");
+      } else if (roundWinner === 'computer') {
+        console.log("The computer won!");
+      } else {
+        console.log("It's a tie.");
+      }
+    },
 
-  playAgain() {
-    console.log('Would you like to play again? (y/n)');
-    let answer = readline.question();
+    displayScore() {
+      console.log(`Your score: ${this.human.getScore()}`);
+      console.log(`Computer score: ${this.computer.getScore()}`);
+    },
 
-    return answer.toLowerCase()[0] === 'y';
-  },
+    displayMatchWinner() {
+      let matchWinner = this.getMatchWinner();
 
-  play () {
-    this.displayWelcomeMessage();
+      console.log(`${matchWinner === 'human' ? "You have" : "The computer has"} won the match with 5 points!`);
+    },
 
-    while (true) {
-      this.human.choose();
-      this.computer.choose();
-      this.displayWinner();
+    displayMoveHistories() {
+      let playerMoves = this.human.getMoveHistory();
+      let computerMoves = this.computer.getMoveHistory();
 
-      if (!this.playAgain()) break;
-    }
+      console.log("Round | Player   | Computer");
 
-    this.displayGoodbyeMessage();
-  },
+      for (let index = 0; index < playerMoves.length; index += 1) {
+        console.log(`${String(index + 1).padStart(3).padEnd(5)} | ${playerMoves[index][0].padEnd(8)} | ${computerMoves[index][0]}`);
+      }
+    },
+
+    hasWinner() {
+      return Math.max(this.human.getScore(), this.computer.getScore()) >= 5;
+    },
+
+    getRoundWinner() {
+      let humanMove = this.human.getMove();
+      let computerMove = this.computer.getMove();
+
+      if (this.winningMoves[humanMove].includes(computerMove)) {
+        return 'human';
+      } else if (this.winningMoves[computerMove].includes(humanMove)) {
+        return 'computer';
+      } else {
+        return null;
+      }
+    },
+
+    getMatchWinner() {
+      if (this.human.getScore() >= 5) {
+        return 'human';
+      } else {
+        return 'computer';
+      }
+    },
+
+    updateScore(roundWinner) {
+      if (roundWinner === 'human') {
+        this.human.incrementScore();
+      } else if (roundWinner === 'computer') {
+        this.computer.incrementScore();
+      }
+    },
+
+    updateMoveHistories(roundWinner) {
+      if (roundWinner === 'human') {
+        this.human.updateMoveHistory("W");
+        this.computer.updateMoveHistory("L");
+      } else if (roundWinner === 'computer') {
+        this.human.updateMoveHistory("L");
+        this.computer.updateMoveHistory("W");
+      } else {
+        this.human.updateMoveHistory("T");
+        this.computer.updateMoveHistory("T");
+      }
+    },
+
+    resetMatch() {
+      this.human.resetScore();
+      this.computer.resetScore();
+    },
+
+    getUserInput(input) {
+      console.log(`${input} (y/n)`);
+      let answer = readline.question().toLowerCase();
+
+      while (!['y', 'n'].includes(answer)) {
+        console.log("Invalid input, try again. (y/n)");
+        answer = readline.question().toLowerCase();
+      }
+
+      return answer === 'y';
+    },
+
+    play () {
+      this.displayWelcomeMessage();
+
+      while (true) {
+        while (!this.hasWinner()) {
+          this.human.choose();
+          this.computer.choose();
+
+          let roundWinner = this.getRoundWinner();
+          this.displayRoundWinner(roundWinner);
+          this.updateScore(roundWinner);
+          this.displayScore();
+          this.updateMoveHistories(roundWinner);
+        }
+
+        this.displayMatchWinner();
+        this.computer.updateWeights();
+        console.log(this.computer.weights);
+        if (this.getUserInput("Would you like to see move histories?")) {
+          this.displayMoveHistories();
+        }
+
+        this.resetMatch();
+
+        if (!this.getUserInput("Would you like to play again?")) break;
+      }
+
+      this.displayGoodbyeMessage();
+    },
+  };
+}
+
+const winningMoves =  {
+  rock: ['scissors', 'lizard'],
+  paper: ['rock', 'spock'],
+  scissors: ['paper', 'lizard'],
+  spock: ['scissors', 'rock'],
+  lizard: ['paper', 'spock'],
 };
+
+let RPSGame = createRPSGame(winningMoves);
 
 RPSGame.play();
---
+++ lesson_1/rps_extras_notes.md
@@ -0,0 +1,29 @@
+## RPS Bonus Features Notes ##
+
+**Keeping Score**
+
+New object type or state of an existing class: within the context of the program, the score for each player is updated after each round, so it makes sense to implement score as part of the state of the player object. 
+
+Within the game, each player's score is updated after each round (winning player's score is incremented by 1, losing player's is unchanged), and if one player has at least 5 points, they win and the game loop ends. Therefore, the engine object should also be responsible for handling the logic of checking which player wins.
+
+Since `score` is part of the state of the player object, instead of directly accessing or changing the `score` property of each player, `RPSGame` will update the score using methods provided by the player object.
+
+**Add Lizard and Spock**
+
+To make the `getRoundWinner` method more readable/easier to maintain, turn the conditionals in the `if` statement into a `winningMoves` collaborator object, where the keys are possible moves, and the corresponding values are an array of losing moves from the other player (for example, `winningMoves.rock = ['scissors', 'lizard']`). This simplifies the conditional to
+
+```javascript
+if (this.winningMoves[humanMove].includes(computerMove)) {
+  return 'human';
+} else if (this.winningMoves[computerMove].includes(humanMove)) {
+  return 'computer';
+} else {
+  return null;
+}
+```
+
+From a design perspective it makes sense for `winningMoves` to be a property of the `RPSGame` object since `RPSGame` is an object that implements the game logic, which includes the possible moves and the interactions between the possible moves. Also, since the `human` and `computer` objects are collaborators of the `RPSGame` object, having the possible moves available through the keys of `winningMoves` allows the `choose` method to 
+
+**Keep Track of a History of Moves**
+
+There are two relevant pieces of information regarding move history: the moves themselves and whether and the result of the round (win, loss or tie). A nested array, where the 1st element of each subarray is the move, and the 2nd element of each subarray is the result, can store this information. After every round, the `moveHistory` property of each player is updated with this information.
